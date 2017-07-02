276366c958d716b2f78438842e3f39b2dd97e8df
melampyge <ozer.duman@gmail.com>
2017-07-02 18:32:33 +0200

    Calculation of area difference related to compression energy.

--- Area/calc_area_diff.cpp
+++ Area/calc_area_diff.cpp
@@ -1,9 +1,9 @@
 
-/* calculate the area of the cells */
+/* calculate the averaged area difference per cell and per simulation */
 
 // COMPILATION AND RUN COMMANDS:
-// g++ -O3 -Wl,-rpath=$HOME/hdf5/lib -L$HOME/hdf5/lib -I$HOME/hdf5/include ${spath}/calc_area_of_cells.cpp ${upath}/read_write.cpp -lhdf5 -lm -o calc_area_of_cells
-// ./calc_area_of_cells out.h5 ${path}/Area_of_cells.txt
+// g++ -O3 -Wl,-rpath=$HOME/hdf5/lib -L$HOME/hdf5/lib -I$HOME/hdf5/include ${spath}/calc_area_of_cells.cpp ${upath}/read_write.cpp -lhdf5 -lm -o calc_area_diff
+// ./calc_area_diff out.h5 ${path}/Area_diff.txt
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -47,8 +47,8 @@ double calc_areas_per_frame (const double * const *x, const double * const *y,
       double areapc = 0.;
       
       for (int j = 0; j < nbpc[n]-1; j++) {
-        areapc += x[step][k]*y[step][k+1] - x[step][k+1]*y[step][k];
-        k++;
+	areapc += x[step][k]*y[step][k+1] - x[step][k+1]*y[step][k];
+	k++;
 	
       }		// nbpc
       
@@ -78,7 +78,7 @@ int main (int argc, char *argv[]) {
  double **x;
  double **y;
  std::string cells_or_beads = "beads";	   // analysis will be done over cells
- bool get_image = false;			   // analysis will be done over images
+ bool get_image = true;			   // analysis will be done over images 
 					   // instead of unwrapped coords
 					    
  // get the file name by parsing and load simulation data as well as positions
@@ -88,7 +88,7 @@ int main (int argc, char *argv[]) {
   
   // print info
    
-  cout << "Calculating areas from " << cells_or_beads << 
+  cout << "Calculating area difference from " << cells_or_beads <<
     " for the following file: \n" << filename << endl;
   cout << "nsteps = " << sim.nsteps << endl;
   cout << "ncells = " << sim.ncells << endl;
@@ -100,26 +100,21 @@ int main (int argc, char *argv[]) {
   
   // perform the analysis
   
-  double avg_area = calc_areas_per_frame(x, y, area_per_frame, 
+  double avg_area_diff = calc_areas_per_frame(x, y, area_per_frame,
 					 sim.nsteps, sim.ncells, sim.nbeads, sim.nbpc);
 					   
   // write the computed data
   
   string outfilepath = argv[2];
-  cout << "Writing average area of the cells to the following file: \n" << outfilepath << endl;  
-  write_single_analysis_data(avg_area, outfilepath);
-  string outfilepath_2 = argv[3];
-  cout << "Writing area of the cells per frame to the following file: \n" << outfilepath_2 << endl;    
-  write_1d_vec_analysis_data(area_per_frame, sim.nsteps, outfilepath_2);
+  cout << "Writing area difference of the cells to the following file: \n" << outfilepath << endl;
+  write_single_analysis_data(avg_area_diff, outfilepath);
   
   // deallocate the arrays
+  //
+  // TODO: I should update this to unique pointers to avoid this deallocation business
   
-  for (int i = 0; i < sim.nsteps; i++) {
-    delete [] x[i];
-    delete [] y[i];
-  }
-  delete [] x;
-  delete [] y;
+  deallocate_2d_array(x, sim.nsteps);
+  deallocate_2d_array(y, sim.nsteps);
   
   return 0;
 }  
